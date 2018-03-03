506cdbcb026e1d0ff2eaa23714acd9104c652b6b
Mauro2196 <mauro2196@gmail.com>
2018-03-03 17:58:20 -0500

    Template

--- temperatura/main.rb
+++ temperatura/main.rb
@@ -2,38 +2,45 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 get "/" do
-    erb :home
+  erb :home
 end
 
 get "/home" do
-    erb :home
+  erb :home
 end
 
 get '/celsius' do
-    erb :celsius
-    
-
-
+  erb :celsius
 end
 
 post '/celsius' do
-    @tCelcius = params[:tCelcius]
-    @tCelcius = ((9/5)* @tCelcius.to_i)+32
-    erb :kelvin
+  @tCelcius = params[:tCelcius]
+  @tCelcius1 = ((1.8) * @tCelcius.to_i) + 32
+  @tCelcius2 = (@tCelcius.to_i) + +273.15
+  erb :celsius
 end
 
-
 get '/farenheit' do
-    erb :farenheit
+  erb :farenheit
+end
+
+post '/farenheit' do
+  @tfar = params[:tfar]
+  @tfar1 = ((@tfar.to_i - 32) * (0.55))
+  p @tfar1.to_i
+  @tfar2 = ((@tfar.to_i + 459.67) * (0.55))
+
+  erb :farenheit
 end
 
 get '/kelvin' do
-    erb :kelvin
+  erb :kelvin
 end
 
+post '/kelvin' do
+  @tkelvin = params[:tkelvin]
+  @tkelvin1 = ((1.8) * (@tkelvin.to_i) - 459.67)
 
-
-
-
-
-
+  @tkelvin2 = (@tkelvin.to_i) - 273.15
+  erb :kelvin
+end
